40ca51eb1dca614df39d78baa3bebbd18cea2d9b
Juanchi36 <juanmartingon@hotmail.com>
2018-10-09 19:44:55 -0300

    first commit

---
+++ src/versionado/ConexionVersionado.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versionado;
+
+/**
+ *
+ * @author juanm
+ */
+public class ConexionVersionado {
+     private String url, nombre, clave;
+    public ConexionVersionado (){
+        this.url = "jdbc:mysql://localhost/estudiantes";
+        this.nombre="root";
+        this.clave="";
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    public java.sql.Connection conectar (){
+        java.sql.Connection conec=null;
+        try{
+            conec=java.sql.DriverManager.getConnection(url, nombre, clave);
+        }
+        catch(java.sql.SQLException ex){
+            System.out.println(ex);
+            
+        }
+        return conec;
+    }
+}
+
---
+++ src/versionado/Versionado.java
@@ -0,0 +1,15 @@
+
+package versionado;
+
+/**
+ *
+ * @author juanm
+ */
+public class Versionado {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+}
